e4b5d5f2fa369389e29da11a92dc95d6fb4312ee
AndreZ <zwieback@crazyuniverse.net>
2024-06-20 23:01:01 +0200

    remove bat files

--- 1start_server.bat
+++
@@ -1,5 +0,0 @@
-cd /D "E:\SteamLibrary\steamapps\common\DayZ"
-
-taskkill /IM DayZDiag_x64.exe /F
-
-start DayZDiag_x64.exe -port=2302 -noPause -forcedebugger -scriptDebug=true -dologs -adminlogs -server "-serverMod=P:\azwMods\@azw_SortingHat" "-mod=E:\SteamLibrary\steamapps\common\DayZ\@stmoritz;P:\azwMods\@azw_SortingHat" "-config=E:\SteamLibrary\steamapps\common\DayZServer\DebugProfile.cfg" "-profiles=E:\SteamLibrary\steamapps\common\DayZServer\Debugprofile" -scrAllowFileWrite -filePatching "-mission=E:\SteamLibrary\steamapps\common\DayZServer\mpmissions\empty.stmoritz" -newErrorsAreWarnings=1
--- 3start_workbench.bat
+++
@@ -1,3 +0,0 @@
-cd /D "E:\SteamLibrary\steamapps\common\DayZ Tools\Bin\Workbench"
-
-start workbenchApp.exe -mod=P:\azw_SortingHat
